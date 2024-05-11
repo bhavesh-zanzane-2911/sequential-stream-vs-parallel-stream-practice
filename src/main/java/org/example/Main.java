64a7bde7dd3545a89bcfb6b16cbe95de5654315b
bhavesh-zanane-2911 <bhaveshzanzane2911@outlook.com>
2024-05-11 21:39:21 +0530

    Bhavesh:Added Log4j2 setup

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,13 @@
 package org.example;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class Main {
+
+    private static final Logger LOG = LoggerFactory.getLogger(Main.class);
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int availableProcessors = Runtime.getRuntime().availableProcessors();
+        LOG.info("availableProcessors : {} ",availableProcessors);
     }
 }
